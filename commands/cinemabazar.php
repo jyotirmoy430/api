777b1c646580a341dade2301d00fa8f7370ac01a
Jyotirmoy Biswas <jyoti@cefalo.com>
2022-09-28 16:19:02 +0600

    New

--- commands/cinemabazar.php
+++ commands/cinemabazar.php
@@ -2,93 +2,98 @@
 
 
 function loopAndTake($parent, $payload, $data){
-    $HOST_ONLY = 'http://cinemabazar.net';
+    try{
+        $HOST_ONLY = 'http://cinemabazar.net';
 
-    if(
-        strpos($parent, "/DATA/NAS1/Tutorial") !== false ||
-        strpos($parent, "/DATA/NAS1/E-Books") !== false ||
-        strpos($parent, "/DATA/NAS1/Games") !== false ||
-        strpos($parent, "/DATA/NAS1/MP3") !== false ||
-        strpos($parent, "/DATA/NAS1/Software") !== false ||
-        strpos($parent, "/DATA/NAS1/WALLPAPER") !== false ||
-        strpos($parent, "/DATA/NAS1/WWE%20%26%20AEW%20Wrestling/") !== false ||
-        strpos($parent, "/DATA/NAS1/WWE %26 AEW Wrestling") !== false ||
-        strpos($parent, "/DATA/NAS1/Documentary") !== false
-    ){
-        return $data;
-    }
-
-
-    $result = call($parent, $payload);
-
-
-    if($result && $result["content"]) {
-        $content = $result["content"];
-
-        $content = json_decode($content);
-
-        if($content) {
-            if ($content && isset($content->items)) {
-                $items = $content->items;
-
-                $parsedPayload = json_decode($payload);
-                foreach($items as $item){
-                    if(
-                        $item->href !== $parsedPayload->items->href &&
-                        strpos($item->href, $parsedPayload->items->href) !== false){
-
-                        if(strpos($item->href, ".jpg") === false &&
-                            (strpos($item->href, ".mp4") !== false ||
-                                strpos($item->href, ".MP4") !== false ||
-                                strpos($item->href, ".avi") !== false ||
-                                strpos($item->href, ".AVI") !== false ||
-                                strpos($item->href, ".MKV") !== false ||
-                                strpos($item->href, ".mkv") !== false)){
-                            array_push($data, $HOST_ONLY.$item->href);
-
-                            echo "<pre>";
-                            print_r($HOST_ONLY.$item->href);
-                            echo "</pre>\n\n\n";
-
-
-                            $data = array_unique($data);
-
-
-                        }else{
-                            $newParent = $HOST_ONLY.$item->href;
-
-
-                            $newPayload = '{"action":"get","items":{"href":"'.$item->href.'","what":1}}';
-
-                            $newItems = loopAndTake($newParent, $newPayload, $data);
-
-                            $newData = array_merge($data, $newItems);
-
-
-                            $data = array_unique($newData);
-
-                        }
-
-                        echo "Word Found!";
-                    } else{
-                        //echo "Word Not Found!";
-                    }
-
-                }
-
-            }
+        if(
+            strpos($parent, "/DATA/NAS1/Tutorial") !== false ||
+            strpos($parent, "/DATA/NAS1/E-Books") !== false ||
+            strpos($parent, "/DATA/NAS1/Games") !== false ||
+            strpos($parent, "/DATA/NAS1/MP3") !== false ||
+            strpos($parent, "/DATA/NAS1/Software") !== false ||
+            strpos($parent, "/DATA/NAS1/WALLPAPER") !== false ||
+            strpos($parent, "/DATA/NAS1/WWE%20%26%20AEW%20Wrestling/") !== false ||
+            strpos($parent, "/DATA/NAS1/WWE %26 AEW Wrestling") !== false ||
+            strpos($parent, "/DATA/NAS1/Documentary") !== false
+        ){
+            return $data;
         }
 
 
+        $result = call($parent, $payload);
+
+
+        if($result && $result["content"]) {
+            $content = $result["content"];
+
+            $content = json_decode($content);
+
+            if($content) {
+                if ($content && isset($content->items)) {
+                    $items = $content->items;
+
+                    $parsedPayload = json_decode($payload);
+                    foreach($items as $item){
+                        if(
+                            $item->href !== $parsedPayload->items->href &&
+                            strpos($item->href, $parsedPayload->items->href) !== false){
+
+                            if(strpos($item->href, ".jpg") === false &&
+                                (strpos($item->href, ".mp4") !== false ||
+                                    strpos($item->href, ".MP4") !== false ||
+                                    strpos($item->href, ".avi") !== false ||
+                                    strpos($item->href, ".AVI") !== false ||
+                                    strpos($item->href, ".MKV") !== false ||
+                                    strpos($item->href, ".mkv") !== false)){
+                                array_push($data, $HOST_ONLY.$item->href.":::".$item->size);
+
+                                echo "<pre>";
+                                print_r($HOST_ONLY.$item->href);
+                                echo "</pre>\n\n\n";
+
+
+                                $data = array_unique($data);
+
+
+                            }else{
+                                $newParent = $HOST_ONLY.$item->href;
+
+
+                                $newPayload = '{"action":"get","items":{"href":"'.$item->href.'","what":1}}';
+
+                                $newItems = loopAndTake($newParent, $newPayload, $data);
+
+                                $newData = array_merge($data, $newItems);
+
+
+                                $data = array_unique($newData);
+
+                            }
+
+                            echo "Word Found!";
+                        } else{
+                            //echo "Word Not Found!";
+                        }
+
+                    }
+
+                }
+            }
+
+
+        }
+        return array_values($data);
+    }catch (\Exception $e){
+
     }
-    return array_values($data);
+
 }
 
 function cinemabazar(){
-    $HOST = 'http://cinemabazar.net/DATA/NAS1/TV%20Series/English%20TV%20Series/Lucifer/';
+    $HOST = 'http://cinemabazar.net/DATA/NAS1/';
 
     $parent = $HOST.'/';
-    $payloadHref = '/DATA/NAS1/TV%20Series/English%20TV%20Series/Lucifer/';
+    $payloadHref = '/DATA/NAS1/';
     $payload = '{"action":"get","items":{"href":"'.$payloadHref.'","what":1}}';
     $parent = str_replace(' ', '%20', $parent);
     $payload = str_replace(' ', '%20', $payload);
@@ -101,32 +106,40 @@ function cinemabazar(){
     if($bigArr){
         foreach($bigArr as $key=>$final){
 
-            $object = new stdClass();
-            $object->id = $key;
-            $object->video = $final;
+            try{
+                $object = new stdClass();
+                $object->id = $key;
+
+                $finalExplode = explode(":::", $final);
+                $object->video = $finalExplode[0];
+                $object->size = $finalExplode[1];
 
 
 
-            if(strpos($final, "2022") !== false){
-                $object->year = 2022;
-            } elseif(strpos($final, "2021") !== false){
-                $object->year = 2021;
-            } elseif(strpos($final, "2020") !== false){
-                $object->year = 2020;
-            } elseif(strpos($final, "2019") !== false){
-                $object->year = 2019;
-            } elseif(strpos($final, "2018") !== false){
-                $object->year = 2018;
-            } elseif(strpos($final, "2017") !== false){
-                $object->year = 2017;
-            } elseif(strpos($final, "2016") !== false){
-                $object->year = 2016;
-            } else{
-                $object->year = 0;
+                if(strpos($final, "2022") !== false){
+                    $object->year = 2022;
+                } elseif(strpos($final, "2021") !== false){
+                    $object->year = 2021;
+                } elseif(strpos($final, "2020") !== false){
+                    $object->year = 2020;
+                } elseif(strpos($final, "2019") !== false){
+                    $object->year = 2019;
+                } elseif(strpos($final, "2018") !== false){
+                    $object->year = 2018;
+                } elseif(strpos($final, "2017") !== false){
+                    $object->year = 2017;
+                } elseif(strpos($final, "2016") !== false){
+                    $object->year = 2016;
+                } else{
+                    $object->year = 0;
+                }
+
+
+                $FULL_FINAL[] = $object;
+            }catch (\Exception $e){
+
             }
 
-
-            $FULL_FINAL[] = $object;
 
         }
     }
