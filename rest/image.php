3bb96a1ab1f6d01a0d2e9f8ffd032188b45f2a39
Jyotirmoy Biswas <jyoti@cefalo.com>
2022-12-06 15:30:34 +0600

    Image

---
+++ rest/image.php
@@ -0,0 +1,56 @@
+<?php
+error_reporting(0);
+
+$keyword = ($_GET["keyword"]) ? str_replace(" ", "%20", $_GET["keyword"]) : "";
+$data = get_web_page("https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=".$keyword);
+$dom = new DomDocument();
+$dom->loadHTML($data["content"]);
+
+$counter = 0;
+$finalSrc = '';
+foreach ($dom->getElementsByTagName('img') as $item) {
+    $counter++;
+    if($counter > 1){
+        $src = $item->getAttribute('src');
+        $finalSrc = $src;
+        break;
+    }
+}
+echo $finalSrc;
+
+
+
+function get_web_page( $url )
+{
+    $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+    $options = array(
+
+        CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
+        CURLOPT_POST           =>false,        //set to GET
+        CURLOPT_USERAGENT      => $user_agent, //set user agent
+        CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+        CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+        CURLOPT_RETURNTRANSFER => true,     // return web page
+        CURLOPT_HEADER         => false,    // don't return headers
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+        CURLOPT_ENCODING       => "",       // handle all encodings
+        CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+        CURLOPT_CONNECTTIMEOUT => 10,      // timeout on connect
+        CURLOPT_TIMEOUT        => 10,      // timeout on response
+        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+    );
+
+    $ch      = curl_init( $url );
+    curl_setopt_array( $ch, $options );
+    $content = curl_exec( $ch );
+    $err     = curl_errno( $ch );
+    $errmsg  = curl_error( $ch );
+    $header  = curl_getinfo( $ch );
+    curl_close( $ch );
+
+    $header['errno']   = $err;
+    $header['errmsg']  = $errmsg;
+    $header['content'] = $content;
+    return $header;
+}
